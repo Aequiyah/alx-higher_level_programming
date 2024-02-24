88f1508b6c9d6848720767df11c011ddfa7273e1
Aequiyah <122805874+Aequiyah@users.noreply.github.com>
2024-02-24 00:41:27 -0800

    8-concat_edges.py

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+str = "Pyhton is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
+print(str[67:82] + str [5:7] +str[0:6])
